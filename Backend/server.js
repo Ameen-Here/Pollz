f66adfdc7aa392cef1d87f06292f586111e1f104
Ameen-Here <johnharry125@yahoo.com>
2022-10-11 09:16:26 +0530

    Init socket io and connected between client and server

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,48 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+// Import HTTP and CORS to allow data transfer
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors());
+
+const port = process.env.PORT || 3001;
+
+const server = app.listen(port, () => console.log(`Listening on port ${port}`));
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
+
+// Initialize options
+const options = {};
+
+// On new client connextion
+io.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+  io.emit("update", options);
+  io.emit("voteCompleted", false);
+
+  // On new vote
+  socket.on("vote", (index) => {
+    // Increase the vote at index
+    if (candidate[index]) {
+      candidate[index].vote += 1;
+    }
+    if (candidate[totalVote] === io.engine.clientsCount - 1) {
+      // Emit voteComplete if all students voted
+      io.emit("voteCompleted", true);
+    }
+    io.emit("update", options);
+  });
+});
+
 // Other middlewares
 app.use(express.static(path.join(__dirname + "/public"))); // To join react app
 app.use(express.json());
@@ -17,7 +59,3 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/connected", (req, res) => {
   res.send({ status: "Successful" });
 });
-
-const port = process.env.PORT || 3001;
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import Header from "./components/header/Header";
 import MainSection from "./components/main-section/MainSection";
 
+import socketIO from "socket.io-client";
+
+const socket = socketIO.connect("http://localhost:3001");
+
 function App() {
   return (
     <div className="app">
